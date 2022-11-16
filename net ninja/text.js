22725a033b620854acf99ca9b45c3d59a16d6b82
jolzy fx codes <105318757+Jolzyfx@users.noreply.github.com>
2022-11-16 16:28:51 +0100

    Add files via upload

---
+++ net ninja/text.js
@@ -0,0 +1,15 @@
+/*let conf = confirm('you gotta be 18 to view this content');
+
+let name = prompt('whats your name?');
+
+let age = prompt('whats your age?')
+    if(age>=18){
+        alert('welcome!');
+    }else{
+        alert(' sorry you GOTTA be 18 or above to apply' );
+    } */
+
+
+
+
+
